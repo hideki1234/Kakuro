6a99dfb4fb2a6ce94995edc3dc19ea3f9fc8f6c8
Hideki Ikeda <hideki.ikeda@gmail.com>
2016-04-23 03:06:29 -0400

    Added meta data UI

--- Editor/metadataview.cpp
+++ Editor/metadataview.cpp
@@ -1,8 +1,101 @@
 #include "metadataview.h"
+#include <QLabel>
+#include <QIntValidator>
+
+std::pair<QSpinBox *, QSpinBox *>
+MetaDataView::makeTimeEdits(const QString &labelText, QBoxLayout *pBL)
+{
+    // input line layout
+    auto pBLLine = new QBoxLayout{QBoxLayout::LeftToRight};
+
+    // level label
+    auto pLbl = new QLabel{labelText, this};
+    pLbl->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+    pBLLine->addWidget(pLbl);
+
+    pBLLine->addStretch();
+
+    // minute
+    QFontMetrics metrics(font());
+
+    auto pEditMin = new QSpinBox{this};
+    pEditMin->setMinimum(0);
+    pEditMin->setMaximum(9999);
+    pEditMin->setFixedWidth(metrics.width("8888") + SPIN_WIDTH);
+    pEditMin->setAlignment(Qt::AlignRight);
+    pBLLine->addWidget(pEditMin);
+
+    pLbl = new QLabel{":"};
+    pBLLine->addWidget(pLbl);
+
+    // second
+    auto pEditSec = new QSpinBox{this};
+    pEditSec->setMinimum(0);
+    pEditSec->setMaximum(59);
+    pEditSec->setFixedWidth(metrics.width("88") + SPIN_WIDTH);
+    pEditSec->setAlignment(Qt::AlignHCenter);
+    pBLLine->addWidget(pEditSec);
+
+    pBL->addLayout(pBLLine);
+
+    return std::make_pair(pEditMin, pEditSec);
+}
 
 MetaDataView::MetaDataView(MetaDataManager *metaData, QWidget *parent)
     : QWidget(parent)
     , m_pMetaData(metaData)
 {
+    // backgournd color
+    QPalette palBack(palette());
+    palBack.setColor(QPalette::Background, Qt::white);
+    setPalette(palBack);
+    setAutoFillBackground(true);
+
+    QBoxLayout *pBLTop = new QBoxLayout{QBoxLayout::TopToBottom, this};
+
+    // Author
+    QLabel *pLbl;
+    pLbl = new QLabel{tr("Author:"), this};
+    pLbl->setAlignment(Qt::AlignLeft);
+    pLbl->setStyleSheet("font-weight:bold");
+    pBLTop->addWidget(pLbl);
+
+    m_pEditAuthor = new QLineEdit{this};
+    m_pEditAuthor->setPlaceholderText(tr("Enter author's name"));
+    m_pEditAuthor->setMaxLength(255);
+    pBLTop->addWidget(m_pEditAuthor);
+
+    pBLTop->addSpacing(20);
+
+    // target times
+    pLbl = new QLabel{tr("Target Times"), this};
+    pLbl->setAlignment(Qt::AlignLeft);
+    pLbl->setStyleSheet("font-weight:bold");
+    pBLTop->addWidget(pLbl);
+
+    // beginner
+    auto timeEdits = makeTimeEdits(tr("Beginner:"), pBLTop);
+    m_pEditBegMin = timeEdits.first;
+    m_pEditBegSec = timeEdits.second;
+
+    // intermediate
+    timeEdits = makeTimeEdits(tr("Intermediate:"), pBLTop);
+    m_pEditIntMin = timeEdits.first;
+    m_pEditIntSec = timeEdits.second;
+
+    // advanced
+    timeEdits = makeTimeEdits(tr("Advanced:"), pBLTop);
+    m_pEditAdvMin = timeEdits.first;
+    m_pEditAdvSec = timeEdits.second;
+
+    // expert
+    timeEdits = makeTimeEdits(tr("Expert:"), pBLTop);
+    m_pEditExpMin = timeEdits.first;
+    m_pEditExpSec = timeEdits.second;
+
+    // stretch
+    pBLTop->addStretch();
+
+    setLayout(pBLTop);
     setMinimumWidth(120);
 }
--- Editor/metadataview.h
+++ Editor/metadataview.h
@@ -3,12 +3,49 @@
 
 #include <QWidget>
 #include "metadatamanager.h"
+#include <QBoxLayout>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <utility>
 
 class MetaDataView : public QWidget
 {
     Q_OBJECT
 
+    /*
+     * dimension
+     */
+    static const int SPIN_WIDTH = 25;
+
+    // model (owned by Main)
     MetaDataManager *m_pMetaData;
+
+    /*
+     * input controls
+     */
+    // author
+    QLineEdit *m_pEditAuthor;
+
+    // beginner target time
+    QSpinBox *m_pEditBegMin;
+    QSpinBox *m_pEditBegSec;
+
+    // intermediate target time
+    QSpinBox *m_pEditIntMin;
+    QSpinBox *m_pEditIntSec;
+
+    // advanced target time
+    QSpinBox *m_pEditAdvMin;
+    QSpinBox *m_pEditAdvSec;
+
+    // expert target time
+    QSpinBox *m_pEditExpMin;
+    QSpinBox *m_pEditExpSec;
+
+    /*
+     * initializations
+     */
+    std::pair<QSpinBox *, QSpinBox *> makeTimeEdits(const QString &labelText, QBoxLayout *pBL);
 
 public:
     explicit MetaDataView(MetaDataManager *metaData, QWidget *parent = 0);
